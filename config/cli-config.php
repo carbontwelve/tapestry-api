627655a562604c9211188e1d6e79a7a449afea96
Simon Dann <simon.dann@halesway.co.uk>
2017-01-27 16:16:25 +0000

    :sparkles: added Doctrine cli config

---
+++ config/cli-config.php
@@ -0,0 +1,23 @@
+<?php
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+if (! ini_get('date.timezone')) {
+    date_default_timezone_set('UTC');
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+$settings = include __DIR__.'/../config/app.php';
+$settings = $settings['doctrine'];
+
+$config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(
+    $settings['meta']['entity_path'],
+    $settings['meta']['auto_generate_proxies'],
+    $settings['meta']['proxy_dir'],
+    $settings['meta']['cache'],
+    false
+);
+
+$em = \Doctrine\ORM\EntityManager::create($settings['connection'], $config);
+
+return ConsoleRunner::createHelperSet($em);
